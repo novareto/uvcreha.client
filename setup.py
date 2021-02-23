5dd0783cef3a933b94dbfaf49b1078b87b699b29
Souheil CHELFOUH <trollfot@gmail.com>
2021-02-23 15:25:16 +0100

    Renaming

--- setup.py
+++ setup.py
@@ -15,29 +15,26 @@ with open('README.rst') as readme_file:
 with open('docs/HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = "" 
+requirements = ""
 
-setup_requirements = "" 
+setup_requirements = ""
 
 setup(
-    name="uvcreha.client",
+    name="reha.client",
     author="UV WEB Community",
     author_email="ck@novareto.de",
     version="0.1.0",
-    description="Example Client for UVC-Reha",
+    description="Example Client for Reha",
     long_description=readme + '\n\n' + history,
     long_description_content_type="text/x-rst",
     url="https://git.bg-kooperation.de",
     install_requires=requirements,
     include_package_data=True,
-    packages=find_packages(include=['uvcreha.client']),
+    packages=find_packages(include=['reha.client']),
     setup_requires=setup_requirements,
     entry_points={
         'fanstatic.libraries': [
-            ' uvcreha.client  = uvcreha.client.app:library',
-        ],
-        'docmanager.plugins': [
-            'client = uvcreha.client'
+            ' reha.client  = reha.client.app:library',
         ],
     },
     test_suite='tests',
